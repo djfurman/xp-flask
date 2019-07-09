87e3d001b8a62d20c2850f29f45d4edaee7d3ef8
Daniel Furman <djfurman@gmail.com>
2019-07-09 19:56:50 -0400

    Add flask basic setup for the API methods.

---
+++ app/articles.py
@@ -0,0 +1,7 @@
+"""Mocks a collection of articles for testing purposes
+"""
+
+articles_collection = [
+    {"name": "getting to know you", "body": "some data"},
+    {"name": "about me", "body": "everything about me"},
+]
--- app/views.py
+++ app/views.py
@@ -1,13 +1,29 @@
-from flask import render_template
+from flask import render_template, jsonify, request
 
-from app import app
+from app import application
+from . import articles
 
 
-@app.route("/")
+@application.route("/")
 def index():
     return render_template("index.html")
 
 
-@app.route("/about")
+@application.route("/about")
 def about():
     return render_template("about.html")
+
+
+@application.route("/api")
+def api_root():
+    return jsonify({"v1": f"{request.base_url}/v1"})
+
+
+@application.route("/api/v1")
+def api_version_one():
+    return jsonify({"articles": f"{request.base_url}/articles"})
+
+
+@application.route("/api/v1/articles")
+def api_v1_fetch_articles_collection():
+    return jsonify({"articles": articles.articles_collection})
