feb499602fc18b28a70c42c7bc47dff6afdc49e1
Daniel Furman <djfurman@gmail.com>
2019-07-09 19:56:50 -0400

    Add flask basic setup for the API methods.

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,9 @@
 from flask import Flask
 
+from . import views # Load the views
 
 # Initialize the app
-app = Flask(__name__, instance_relative_config=True)
+application = Flask(__name__, instance_relative_config=True)
 
-# Load the views
-from app import views
 # Load the config file
-app.config.from_object("config")
+application.config.from_object("config")
